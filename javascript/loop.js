542235c4492aee321539bb14b4477541d4278f66
Priyansu <spriyansumeitei@gmail.com>
2026-02-04 21:42:53 +0530

    dom with loop

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,13 +1,36 @@
 // loop 
 // for loop
 
-let a = [1, 2, 3, 4, 5];
+// let a = [1, 2, 3, 4, 5];
 
-for (let i=0; i<a.length; i++) {
+// for (let i=0; i<a.length; i++) {
 
-  console.log(a[i]);
+//   console.log(a[i]);
 
-}
+// }
 
 // print even number (1-20)
 // print odd number (1-20)
+
+
+// DOM manipulation (Document Object Model)
+const welcome = document.getElementById("welcome");
+welcome.innerText = `Welcome to JavaScript Online Class`;
+
+
+const topic = document.getElementById("topic");
+topic.innerText = `DOM Manipulation`;
+
+
+const loopDOM = document.getElementById("loopDOM");
+for (let i = 0; i < 3; i++) {
+  loopDOM.innerHTML += `<h1 style="color: red">JavaScript</h1>`;
+}
+
+
+const test = document.getElementById("test");
+for (let i = 0; i < 4; i++) {
+  test.innerHTML += `<h2>Test 1</h2>`;
+}
+
+
